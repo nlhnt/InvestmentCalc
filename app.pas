57a12b77968d99c768f5e42ba6bc1aeac456b223
nlhnt <grimonce@gmail.com>
2025-07-29 17:50:34 +0200

    trying offcanvas manually

--- app.pas
+++ app.pas
@@ -6,13 +6,14 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Grids,
-  investment_logic;
+  ComCtrls, ExtCtrls, investment_logic;
 
 type
 
   { TinvestmentCalculatorForm }
 
   TinvestmentCalculatorForm = class(TForm)
+    ToggleButton: TButton;
     calculateButton: TButton;
     initInvestmentInput: TEdit;
     annoInvestmentInput: TEdit;
@@ -22,14 +23,24 @@ type
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
+    Label5: TLabel;
+    PageControl1: TPageControl;
+    Panel1: TPanel;
     resultGrid: TStringGrid;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    Timer1: TTimer;
     procedure calculateButtonClick(Sender: TObject);
     procedure initInvestmentInputChange(Sender: TObject);
     procedure expectedReturnInputChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure Label1Click(Sender: TObject);
+    procedure Label5Click(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure ToggleButtonClick(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
-
+         AnimatingOut: Boolean;
   public
 
   end;
@@ -100,6 +111,13 @@ end;
 
 procedure TinvestmentCalculatorForm.FormCreate(Sender: TObject);
 begin
+
+     // Offcanvas setting
+     Panel1.Left := 0;
+     Panel1.Visible:= False;
+     Panel1.Width := 0;
+     AnimatingOut := False;
+     // Compute investment result grid
     resultGrid.ColCount := 6;
     resultGrid.RowCount := 2;  // 1 header + 1 empty data row
 
@@ -114,6 +132,53 @@ end;
 procedure TinvestmentCalculatorForm.Label1Click(Sender: TObject);
 begin
 
+end;
+
+procedure TinvestmentCalculatorForm.Label5Click(Sender: TObject);
+begin
+
+end;
+
+procedure TinvestmentCalculatorForm.FormResize(Sender: TObject);
+begin
+    Panel1.Height := ClientHeight;
+end;
+
+procedure TinvestmentCalculatorForm.ToggleButtonClick(Sender: TObject);
+begin
+    AnimatingOut := Panel1.Visible;
+    Timer1.Enabled := True;
+    Dialogs.ShowMessage('TO Button Clicked!');
+end;
+
+procedure TinvestmentCalculatorForm.Timer1Timer(Sender: TObject);
+const
+  StepSize = 20;
+begin
+    if AnimatingOut then
+        begin
+          // Slide out to left
+          if Panel1.Width > 0 then
+             Panel1.Width := Panel1.Width - Stepsize
+          else
+            begin
+              Panel1.Visible := False;
+              Timer1.Enabled := False;
+            end;
+        end
+    else
+    begin
+     // Slide in from left
+     if not Panel1.Visible then
+        begin
+          Panel1.Width := 0;
+          Panel1.Visible := True;
+        end;
+     if Panel1.Width < 200 then
+        Panel1.Width := Panel1.Width + StepSize
+     else
+       Timer1.Enabled := False;
+    end;
 end;
 
 end.
